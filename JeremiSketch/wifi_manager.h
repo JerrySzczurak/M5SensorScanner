5ac17d43f33d6d46cd7cf457cb0880d134e9825e
jszczurak <jszczurak@ossur.com>
2026-09-24 14:25:18 +0100

    no message

--- JeremiSketch/wifi_manager.h
+++ JeremiSketch/wifi_manager.h
@@ -2,8 +2,8 @@
 #include <Arduino.h>
 
 // === UZUPELNIJ SWOJE DANE WIFI ===
-#define WIFI_SSID "VodafoneADC5A4"
-#define WIFI_PASS "HZ9FgFpMCpJgxarN"
+#define WIFI_SSID "UseYours"
+#define WIFI_PASS "UseYours"
 
 // Czas miedzy kolejnymi probami polaczenia gdy WiFi niedostepne (ms)
 #define WIFI_RETRY_INTERVAL_MS 30000
